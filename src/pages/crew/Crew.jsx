f3ba1b3e3ee6ba18403f7c5ac3566b5a6d0fe4fa
svkhrobbeck <svkhrobbeck@gmail.com>
2023-08-21 22:14:07 +0500

    Update crew page and styles, add useSearchParams hook

--- src/pages/crew/Crew.jsx
+++ src/pages/crew/Crew.jsx
@@ -1,8 +1,48 @@
 // styles
 import "./Crew.scss";
 
+import { crew } from "../../data/crew.json";
+import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import useParams from "../../helpers/useParams";
+
 const Crew = () => {
-  return <div>Crew</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const selected = crew.find(item => item.name === searchParams.get("name"));
+  const [selectedCrew, setSelectedCrew] = useState(selected || crew[0]);
+
+  const handleSetCrew = crew => {
+    setSearchParams(useParams(searchParams, "name", crew.name));
+    setSelectedCrew(crew);
+  };
+
+  selectedCrew;
+  return (
+    <section className="crew">
+      <div className="container">
+        <h2 className="crew__title title">
+          <b>02</b> Meet your crew
+        </h2>
+        <div className="container--mini crew__container">
+          <div className="crew__content">
+            <h2 className="crew__role">{selectedCrew.role}</h2>
+            <h3 className="crew__name">{selectedCrew.name}</h3>
+            <p className="crew__bio">{selectedCrew.bio}</p>
+            <ul className="crew__paginations list">
+              {crew.map(item => (
+                <li
+                  className={`crew__pagination ${selectedCrew.name === item.name ? "active" : ""}`}
+                  key={item.name}
+                  onClick={() => handleSetCrew(item)}
+                />
+              ))}
+            </ul>
+          </div>
+          <img className="crew__img" src={selectedCrew.images.png} alt={selectedCrew.name} srcSet={selectedCrew.images.png2x} />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Crew;
