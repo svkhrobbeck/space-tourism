b5b2a61b64cd82519ab8048e667ad12a9b0388f6
svkhrobbeck <svkhrobbeck@gmail.com>
2023-08-21 21:32:50 +0500

    Add components' exporter index

---
+++ src/components/index.jsx
@@ -0,0 +1,3 @@
+export { default as Header } from "./header/Header";
+export { default as Explore } from "./explore/Explore";
+export { default as DestinationHero } from "./destination-hero/DestinationHero";
