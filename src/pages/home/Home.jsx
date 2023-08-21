7a24a8ce634ec33d73afcda975fcbbcab90d1969
svkhrobbeck <svkhrobbeck@gmail.com>
2023-08-21 21:29:40 +0500

    Create home page

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+// styles
+import "./Home.scss";
+
+import { Explore } from "../../components";
+
+const Home = () => {
+  return (
+    <>
+      <Explore />
+    </>
+  );
+};
+
+export default Home;
