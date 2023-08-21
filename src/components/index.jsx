a4a761ef5ccfd89b62c21eda479754c988adc4bd
svkhrobbeck <svkhrobbeck@gmail.com>
2023-08-21 21:40:06 +0500

    Delete destination-hero and explore components

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,1 @@
 export { default as Header } from "./header/Header";
-export { default as Explore } from "./explore/Explore";
-export { default as DestinationHero } from "./destination-hero/DestinationHero";
