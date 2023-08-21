0c919ea5b53f74802b26da9d2a63fad70e6b8c43
svkhrobbeck <svkhrobbeck@gmail.com>
2023-08-21 21:28:37 +0500

    Add styles to MainLayout

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,15 +1,21 @@
+// styles
+import "./MainLayout.scss";
+
 import Router from "../router/Router";
-import { Footer, Header } from "../components";
+import { Header } from "../components";
+import { useLocation } from "react-router-dom";
+import setBg from "../helpers/setBg";
 
 const MainLayout = () => {
+  const { pathname } = useLocation();
+
   return (
-    <>
+    <div className={`bg ${setBg(pathname)}`}>
       <Header />
-      <main className="main-layout">
+      <main className="main-content">
         <Router />
       </main>
-      <Footer />
-    </>
+    </div>
   );
 };
 
