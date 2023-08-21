b94f96f5d6c903c27bf6fce8cb458978b4b825c7
svkhrobbeck <svkhrobbeck@gmail.com>
2023-08-21 23:58:24 +0500

    Make responsive header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,15 +3,23 @@ import "./Header.scss";
 
 import { Link, NavLink } from "react-router-dom";
 import { sitenavLinks } from "../../helpers/constants";
+import { useState } from "react";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="site-header">
       <div className="site-header__container">
         <Link className="site-header__logo logo" to="/">
           <img className="logo__img" src="/logo.svg" alt="Space tourism logo" width={48} height={48} />
         </Link>
-        <nav className="site-header__sitenav sitenav">
+        <button className={`button site-header__toggler ${isOpen ? "site-header__toggler--open" : ""}`} onClick={() => setIsOpen(p => !p)}>
+          <span className="site-header__toggler-inner"></span>
+          <span className="site-header__toggler-inner"></span>
+          <span className="site-header__toggler-inner"></span>
+        </button>
+        <nav className={`site-header__sitenav sitenav ${isOpen ? "sitenav--open" : ""}`}>
           <ul className="sitenav__list list">
             {sitenavLinks.map(link => (
               <li className="sitenav__item" key={link.id}>
