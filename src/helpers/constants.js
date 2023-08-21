05b28060306d728328ab59fa5d926ec6acb30bde
svkhrobbeck <svkhrobbeck@gmail.com>
2023-08-21 21:31:08 +0500

    Ccreate constants and sitenavLinks variable

---
+++ src/helpers/constants.js
@@ -0,0 +1,6 @@
+export const sitenavLinks = [
+  { id: "01", to: "/", text: "HOME" },
+  { id: "02", to: "/destination", text: "DESTINATION" },
+  { id: "03", to: "/crew", text: "CREW" },
+  { id: "04", to: "/technology", text: "TECHNOLOGY" },
+];
