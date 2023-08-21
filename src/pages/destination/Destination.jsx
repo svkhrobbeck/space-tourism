29b12444d6a641ba4aaa05e011e1a3f9355556e3
svkhrobbeck <svkhrobbeck@gmail.com>
2023-08-21 21:29:51 +0500

    Create destination page

---
+++ src/pages/destination/Destination.jsx
@@ -0,0 +1,14 @@
+// styles
+import "./Destination.scss";
+
+import { DestinationHero } from "../../components";
+
+const Destination = () => {
+  return (
+    <>
+      <DestinationHero />
+    </>
+  );
+};
+
+export default Destination;
