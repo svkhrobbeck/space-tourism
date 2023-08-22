d6e06eea0f1fec309345203776122a8ec230fd81
svkhrobbeck <svkhrobbeck@gmail.com>
2023-08-22 14:08:04 +0500

    Update header, Fix hamburger bar bugs

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,13 +8,16 @@ import { useState } from "react";
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const closeSitenav = () => setIsOpen(false);
+  const toggleSitenav = () => setIsOpen(p => !p);
+
   return (
     <header className="site-header">
       <div className="site-header__container">
-        <Link className="site-header__logo logo" to="/">
+        <Link className="site-header__logo logo" to="/" onClick={closeSitenav}>
           <img className="logo__img" src="/logo.svg" alt="Space tourism logo" width={48} height={48} />
         </Link>
-        <button className={`button site-header__toggler ${isOpen ? "site-header__toggler--open" : ""}`} onClick={() => setIsOpen(p => !p)}>
+        <button className={`button site-header__toggler ${isOpen ? "site-header__toggler--open" : ""}`} onClick={toggleSitenav}>
           <span className="site-header__toggler-inner"></span>
           <span className="site-header__toggler-inner"></span>
           <span className="site-header__toggler-inner"></span>
@@ -22,7 +25,7 @@ const Header = () => {
         <nav className={`site-header__sitenav sitenav ${isOpen ? "sitenav--open" : ""}`}>
           <ul className="sitenav__list list">
             {sitenavLinks.map(link => (
-              <li className="sitenav__item" key={link.id}>
+              <li className="sitenav__item" key={link.id} onClick={closeSitenav}>
                 <NavLink className="sitenav__link link" to={link.to}>
                   {" "}
                   <b>{link.id}</b> {link.text}
